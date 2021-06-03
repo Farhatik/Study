c6d88162720afe406b4f33bc4d059c40ad0f6dac
Farkhat Agaev <farkhatagaev@Farkhats-MacBook-Pro.local>
2021-06-04 02:25:08 +0500

    bot

---
+++ HSE/year3/educative_bot/config.py
@@ -0,0 +1,1 @@
+TOKEN = '1529712763:AAF-v-gV_ul1oyTOcfu4HsVjKhDfoulmY-s'
--- HSE/year3/educative_bot/test/test.py
+++ HSE/year3/educative_bot/test/test.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
+
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
